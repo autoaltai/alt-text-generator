c5dd8f64c900b4c0e2a1253a1f232e9b88409fc9
AutoAlt.ai <support@autoalt.ai>
2026-08-12 15:42:03 +0530

    Release version 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 defined('TYPO3') || die();
 
 $EM_CONF[$_EXTKEY] = [
@@ -11,16 +9,16 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'AutoAlt.ai',
     'author_company' => 'AutoAlt.ai',
     'author_email' => 'support@autoalt.ai',
-    'state' => 'alpha',
+    'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '0.1.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.99.99',
-            'typo3' => '14.0.0-14.99.99',
-            'backend' => '14.0.0-14.99.99',
-            'filelist' => '14.0.0-14.99.99',
-            'fluid' => '14.0.0-14.99.99',
+            'typo3' => '13.0.0-14.3.99',
+            'backend' => '13.0.0-14.3.99',
+            'filelist' => '13.0.0-14.3.99',
+            'fluid' => '13.0.0-14.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
